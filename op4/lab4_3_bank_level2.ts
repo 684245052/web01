5cef83a018dbd43c3833db14026c274fd0890cab
684245052 <684245052@webmail.npru.ac.th>
2026-07-08 11:18:38 +0700

    ส่งงานworkshop4

---
+++ op4/lab4_3_bank_level2.ts
@@ -0,0 +1,13 @@
+class BankAccount2 {
+    ownerName: string;
+    balance: number;
+    constructor(ownerName: string, initialBalance: number) {
+        this.ownerName = ownerName;
+        this.balance = initialBalance;
+    }
+    displayBalance(): void {
+        console.log(`Account: ${this.ownerName}, Balance: $${this.balance}`);
+    }
+}
+const accountL1 = new BankAccount("Alice", 500);
+accountL1.displayBalance();
