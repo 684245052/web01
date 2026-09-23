6841c4d0f7c0a6f417503f54d9a34238fc5c944d
684245052 <684245052@webmail.npru.ac.th>
2026-09-23 22:49:53 +0700

    ส่งงานassignment8-9

---
+++ Assignment8-9/Assignment8.ts
@@ -0,0 +1,37 @@
+class Monster {
+    constructor(public name: string, public health: number) {}
+    
+    attack(): void {
+        console.log(`${this.name} uses a basic attack!`);
+    }
+}
+
+class FireMonster extends Monster {
+    attack(): void {
+        console.log(`${this.name} uses Flamethrower! (Fire Effect 🔥)`);
+    }
+}
+
+class WaterMonster extends Monster {
+    attack(): void {
+        console.log(`${this.name} uses Hydro Pump! (Water Effect 💧)`);
+    }
+}
+
+class GrassMonster extends Monster {
+    attack(): void {
+        console.log(`${this.name} uses Solar Beam! (Grass Effect 🌿)`);
+    }
+}
+
+function battleArena(monsters: Monster[]): void {
+    monsters.forEach(monster => monster.attack());
+}
+
+const arenaMonsters: Monster[] = [
+    new FireMonster("Charizard", 100),
+    new WaterMonster("Blastoise", 120),
+    new GrassMonster("Venusaur", 110)
+];
+
+battleArena(arenaMonsters);
